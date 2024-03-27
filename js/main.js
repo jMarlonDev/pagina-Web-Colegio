7f63c7574481f8fe29b851de503abc92e89fb8d5
Jhon Marlon <jmarloncin07@gmail.com>
2024-03-27 11:42:52 -0500

    haciendo la estructura de la pagina web del colegio y dandole estilos

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+//agreganado la clase .visible a la navegacion para que se abra o se oculte con el menu desplegable
+const navegacion = document.querySelector("#navegacion");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+
+abrir.addEventListener("click", () => {
+    navegacion.classList.add("visible");
+});
+
+cerrar.addEventListener("click", ()=>{
+    navegacion.classList.remove("visible")
+});
+
+// agregando click de las fotos de la galeria de inicio
+
+document.querySelectorAll(".imagen-contenido img").forEach(imagen=>{
+    imagen.onclick = () =>{
+        document.querySelector(".imagen-click").style.display = "block";
+        document.querySelector(".imagen-click img").src = imagen.getAttribute("src");
+    }
+});
+
+document.querySelector(".imagen-click span").onclick = () =>{
+    document.querySelector(".imagen-click").style.display = "none";
+}
